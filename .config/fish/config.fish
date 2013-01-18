a35eac634cfde2225fd9bf60fb35503c28f91a9d
Matthew Bunday <zenceph@gmail.com>
2013-01-18 17:13:58 -0500

    Git prompt for fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -22,3 +22,25 @@ alias release-qa-signoff="php ~/s/devtools/workflow/release-qa-signoff.php $1"
 alias release-merge="php ~/s/devtools/workflow/release-merge.php $1"
 alias release-deploy="php ~/s/devtools/workflow/release-deploy.php $1"
 alias pullreq="php ~/s/devtools/workflow/pullreq.php"
+
+set fish_git_dirty_color red
+function parse_git_dirty 
+    git diff --quiet HEAD ^&-
+    if test $status = 1
+        echo (set_color $fish_git_dirty_color)"*"(set_color normal)
+    end
+end
+
+function parse_git_branch
+    # git branch outputs lines, the current branch is prefixed with a *
+    set -l branch (git branch --color ^&- | awk '/*/ {print $2}') 
+    echo $branch (parse_git_dirty)
+end
+
+function fish_prompt
+    if test -n (parse_git_branch)
+        printf '%s%s%s %s> ' (set_color $fish_color_cwd) (prompt_pwd) (set_color normal) (parse_git_branch)            
+    else
+        printf '%s%s%s> ' (set_color $fish_color_cwd) (prompt_pwd) (set_color normal)
+    end 
+end
